8acdb36c1010099159b057a47f5398ff062b6c40
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-06-10 21:44:56 -0700

    setInterval Time

--- script.js
+++ script.js
@@ -22,4 +22,45 @@ function emptyTextBars() {
 function currentTime() {
     var date = moment().format('MMMM Do YYYY, h:mm:ss a');
     
+    if(moment().format("mm:ss") === "00:00") location.reload();
 }
+
+// Comparing the index Values to current hour and return a class name.
+function checkingTimeline(hourIndex) {
+    var currentHour = moment().format("H");
+    return (hourIndex < currentHour ? "past" : (hourIndex > currentHour ? "future" : "present"));
+}
+
+
+
+// Creating a function to save user inputs into local storage.
+function saveContents() {
+    var savedHourlyContent = [];
+    for(var i = 0; i < workHours; i++ ) {
+        savedHourlyContent.push($(`#${i}hourText`).val());
+    }
+    localStorage.setItem("savedHourlyContent", JSON.stringify(savedHourlyContent));
+}
+
+function showClick(target) {
+    target.classList.add("clicked");
+    var clickedDisplay = setTimeout(function() {
+        target.classList.remove("clicked");
+    }, 150);
+}
+// save data when button is clicked 
+function clickedSaveButton(event) {
+    if($(event.target).hasClass("saveID")) {
+        saveContents();
+        (event.target.nodeName === "DIV" ? showClick(event.target) : showClick(event.target.parentNode));
+    }
+}
+
+
+// Updating time by using setInterval
+currentTime();
+var updatingTimeInterval = setInterval(currentTime, 1000); 
+
+
+
+
