b073caaf72d9eaf87687652b48d52073a0cfbfc0
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-06-10 20:46:13 -0700

    created golable varibales and data

---
+++ script.js
@@ -0,0 +1,7 @@
+// Creating Golable variables for current date and hours box
+
+var currentDate = $("#currentDay");
+var hourContainerEl = $("#hourContainer");
+// set hours for daily working and time to start working in the day.
+var workHours = 8;
+var startHour = 10;
