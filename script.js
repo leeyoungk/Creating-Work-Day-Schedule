07988d3430e5abdc2b0dc77bd9c1039a793a3749
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-06-10 21:24:12 -0700

    Created a funtion for local storage and current Time

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ var hourContainerEl = $("#hourContainer");
 // set hours for daily working and time to start working in the day.
 var workHours = 8;
 var startHour = 10;
+
+// creating local Storage for data
+var unsavedHourlyItem = JSON.parse(localStorage.getItem("savedHourlyItem")) || emptyTextBars();
+
+// Creating a function for user to input data
+function emptyTextBars() {
+    var emptyArray = [];
+    for ( var i = 0; i < workHours; i++) {
+        emptyArray.push("");
+    }
+    return emptyArray;
+}
+
+// Creating a function to run current Date.
+function currentTime() {
+    var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    
+}
